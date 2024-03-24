f9d6f10d2f210424b7239066b5c53ee6f4540d45
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-25 05:25:53 +0700

    send API

--- public/script.js
+++ public/script.js
@@ -1,8 +1,53 @@
-var board2 = Chessboard('board2', {
+//Send data when piece is moved
+const onChange = (oldPos, newPos) => {
+    const newArrPos = Object.entries(newPos).sort();
+    const oldArrPos = Object.entries(oldPos).sort();
+
+    let move;
+
+    for(let i = 0; i < newArrPos.length; i++){
+        if(oldArrPos[i][0] != newArrPos[i][0]){
+            move = {
+                from: oldArrPos[i][0],
+                to: newArrPos[i][0],
+                piece: newArrPos[i][1]
+            }
+
+            console.log(move); 
+            break;
+        }
+    }
+}
+//End send data when piece is moved
+
+//Create a new 
+const config = {
     draggable: true,
     dropOffBoard: 'trash',
-    sparePieces: true
-  })
+    position: 'start',
+    onChange: onChange // fires when the board position changes.
+}
+let newBoard = Chessboard('board', config);
+newBoard.start();
+$('#clearBtn').on('click', newBoard.start)
+
+//Show position
+const showPositionBtn = document.getElementById('showPositionBtn');
+if(showPositionBtn){
+    showPositionBtn.addEventListener('click', () => {
+        console.log(newBoard.position());
+      
+        console.log(newBoard.fen());
+    })
+}
+//End show position
+
+//Move piece
+$('#move1Btn').on('click', () => {
+    newBoard.move('e2-e4')
+})
   
-  $('#startBtn').on('click', board2.start)
-  $('#clearBtn').on('click', board2.clear)
+$('#move2Btn').on('click', () => {
+    newBoard.move('d2-d4', 'g8-f6')
+})
+//End move piece
